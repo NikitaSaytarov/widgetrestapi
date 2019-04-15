44159ba834f3829c0428a1dc07cefcc5e72358f4
Никита <rssnikki@outlook.com>
2019-04-15 15:26:58 +0500

    initial

---
+++ src/main/java/com/miro/widgetrestapi/WidgetrestapiApplication.java
@@ -0,0 +1,13 @@
+package com.miro.widgetrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WidgetrestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WidgetrestapiApplication.class, args);
+    }
+
+}
